dec29477d5c98090f1e7e6ccb16af2275685489b
Shivkanta Nagargoje <125877363+shivkantaN@users.noreply.github.com>
2023-05-15 19:20:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,45 @@
-function letterCombinations(input_digit) {
+
   //Complete the function
+	function letterCombinations(digits) {
+  const digitToLetters = {
+    '2': 'abc',
+    '3': 'def',
+    '4': 'ghi',
+    '5': 'jkl',
+    '6': 'mno',
+    '7': 'pqrs',
+    '8': 'tuv',
+    '9': 'wxyz'
+  };
+
+  const result = [];
+
+  function backtrack(combination, nextDigits) {
+    if (nextDigits.length === 0) {
+      result.push(combination);
+    } else {
+      const currentDigit = nextDigits[0];
+      const letters = digitToLetters[currentDigit];
+
+      for (let i = 0; i < letters.length; i++) {
+        const letter = letters[i];
+        backtrack(combination + letter, nextDigits.slice(1));
+      }
+    }
+  }
+
+  if (digits.length !== 0) {
+    backtrack('', digits);
+  }
+
+  return result;
+}
+
+// Example usage:
+const input = "23";
+const output = letterCombinations(input);
+console.log(output); // Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+
 }
 
 module.exports = letterCombinations;
